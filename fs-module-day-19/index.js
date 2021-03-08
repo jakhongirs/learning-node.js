467d594c671f073d2607ab99e438630477d7804b
jakhongirs <j.sagdullaev31@gmail.com>
2021-03-08 23:51:56 +0500

    fs-module-promised-based

---
+++ fs-module-day-19/index.js
@@ -0,0 +1,16 @@
+const fs = require("fs")
+
+fs.readFile("./data.txt", "utf8", (err, data) => {
+  if(err){
+    console.log(err);
+  } 
+  
+  else{
+    console.log(data);
+  }
+})
+
+/* Bu bizga async promised based o'qish hisoblanadi ya'ni tepadagi file-ni ya'ni datani topa olsagina keyin 
+pastdagi utib if yoki then desak ham bo'ladi o'shalarni o'qib beradi. */
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
