ec13cd762b388e0317bcfb687d80889d8c073bab
jakhongirs <j.sagdullaev31@gmail.com>
2021-03-17 17:36:17 +0500

    upload files with html and express

---
+++ upload-file-day-26/index.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const fileUpload = require("express-fileupload")
+const path = require("path")
+/* const { v4 } = require("uuid") */
+
+const app = express()
+app.use("/uploads",express.static(path.join(__dirname,"uploads")))
+
+app.get("/", (req, res) => {
+  res.send("Home Page")
+})
+
+app.use(fileUpload())
+
+app.post("/upload", (req, res) => {
+  const file = req.files.userPhoto
+
+  const filePath = path.join(__dirname, "/uploads", Date.now() + file.name)
+  if(file.mimetype === "image/png") {
+    
+    file.mv(filePath, (err) => {
+      console.log(err)
+    })
+
+    res.status(201).send({ message: "Created", })
+  } 
+  else{
+    res.status(401).end()
+  }
+})
+
+const PORT = process.env.PORT || 4000
+app.listen(PORT, () => console.log(PORT));
